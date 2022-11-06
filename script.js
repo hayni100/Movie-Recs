5efa38574c2baa4310eed9790b872a90e81927f7
Rhys Smoker <r.smoker@gmail.com>
2022-11-06 13:19:44 -0800

    trying to console log infor from the genre object

---
+++ script.js
@@ -0,0 +1,80 @@
+//Outline for Movie Recs
+// 1. User selects some genres using a form submit
+// 2. A queryURL specifying those genres fetches titles from the ADVANCED MOVIE SEARCH API
+// 3. A new queryURL specifying those titles (using title IDs) fetches watchmode ID's from WATCHMODE API
+// 4. A new queryURL specifying those movie ID's fetches "where to stream" info from WATCHMODE API
+//"Where to stream" appears in the DOM
+
+//Advanced Movie Search
+
+var genreCode = 35; // will be a number, for example comedy= 35
+
+function getByGenre() {
+	//gets a number of title IDs'
+	var genreCode = 35; //this will be passed here from user input
+	var genreURL =
+		"https://advanced-movie-search.p.rapidapi.com/discover/movie?with_genres=" +
+		genreCode +
+		"&page=1";
+
+	const options = {
+		method: "GET",
+		headers: {
+			"X-RapidAPI-Key": "ab5fb0b08dmsh801b30df51c049dp15ea7ejsn09d021675790",
+			"X-RapidAPI-Host": "advanced-movie-search.p.rapidapi.com",
+		},
+	};
+
+	fetch(genreURL, options)
+		// 	.then((response) => response.json())
+		// 	.then((response) => console.log(response))
+		// 	.catch((err) => console.error(err));
+
+		.then(function (response) {
+			if (!response.ok) {
+				throw response.json();
+			}
+			return response.json();
+		})
+		.then(function (genreObject) {
+			console.log(genreObject);
+		});
+	console.log(genreObject.results[0].id); //i'm just trying to see the id number, which should be 675054, of the 0 index of the results array.
+}
+getByGenre(); //don't forget to run the function.
+
+// var numOfTitles = 10;
+// for (let i = 0; i < numOfTitles; i++) {
+// 	var titleCode = genreObject.results[i].id;
+// 	var title = genreObject.results[i].title;
+// 	console.log(titleCode);
+// 	console.log(title);
+// }
+
+//Watchmode OTT STREAMING SERVICE AVAILABILITY
+////////////////////////////////////////////////
+// const options2 = {
+// 	method: "GET",
+// 	headers: {
+// 		"X-RapidAPI-Key": "ab5fb0b08dmsh801b30df51c049dp15ea7ejsn09d021675790",
+// 		"X-RapidAPI-Host": "watchmode.p.rapidapi.com",
+// 	},
+// };
+
+// var movie1 = ""; //individual source ID for Watchmode
+// var movie2 = ""; //individual source ID for Watchmode
+// var movie3 = ""; //individual source ID for Watchmode
+
+// var getWatchId =
+// 	"https://watchmode.p.rapidapi.com/list-titles/?types=movie&regions=US&source_types=sub%2Cfree&source_ids=" +
+// 	movie1 +
+// 	"%2C" +
+// 	movie2 +
+// 	"%2C+" +
+// 	movie3 +
+// 	"&page=1&limit=10&genres=4%2C9&sort_by=relevance_desc&network_ids=1%2C8%2C12&release_date_start=20010101&release_date_end=20201211"; //this URL takes 3 movie titles into the fetch URL
+
+// fetch(getWatchId, options2) //uses the fetch URL from above
+// 	.then((response) => response.json())
+// 	.then((response) => console.log(response))
+// 	.catch((err) => console.error(err));
