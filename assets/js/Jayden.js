594b5339283592d5a935bf544c5ba6b30b15e2e0
Jayden Chapman <jaychapmancode@gmail.com>
2022-11-08 21:12:16 -0800

    works on jayden.js

---
+++ assets/js/Jayden.js
@@ -0,0 +1,11 @@
+var sectionEl = document.getElementsByClassName("poster");
+var watchItButton = document.getElementById("watch-movie");
+function printConsole() {
+	console.log("test");
+}
+
+watchItButton.addEventListener("click", printConsole);
+
+function store() {
+    localStorage.setItem("")
+}
