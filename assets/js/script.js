074d2caa661614fa45ecde928b0f31af7d749bdd
Jayden Chapman <jaychapmancode@gmail.com>
2022-11-08 18:26:45 -0800

    cleans up script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,4 +4,3 @@
 // 2. A queryURL specifying those genres fetches titles from the ADVANCED MOVIE SEARCH API
 // 3. A new queryURL specifying those titles fetches watchmode ID's from WATCHMODE API
 // 4. A new queryURL specifying those movie ID's fetches "where to stream" info from WATCHMODE API
-//"Where to stream" shows in the DOM
