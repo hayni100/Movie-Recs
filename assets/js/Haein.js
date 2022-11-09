e78e58822afa89312927379f048dc9d0c4cb5229
Haein Kim <hayni100@gmail.com>
2022-11-08 21:01:13 -0800

    added on-click on js

---
+++ assets/js/Haein.js
@@ -0,0 +1,6 @@
+var remote = document.querySelector("#remote");
+var allButtons = document.querySelector("#all-buttons");
+
+remote.addEventListener("click", function() {
+allButtons.scrollIntoView(true);
+});
