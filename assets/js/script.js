690e56c7c5efcd731dc265420a0e623628d04378
Rhys Smoker <r.smoker@gmail.com>
2022-11-13 22:43:06 -0800

    deleted a problem var that wasn't doing anything helpful on line 47, made lots of comments about what the code COULD do when the API gives bad data

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,19 @@
 var buttonContainerEl = document.querySelector("#all-buttons");
 var posterPath = null; //posterPath gets used during getTitleByGenre but needs to be global, so its up here.
-
-
-//localStorage.clear(); 
-function createEmptyStorage() {
-localStorage.setItem("genreIds","[]")
+localStorage.clear(); //clears all local storage on page load. 
+function createEmptyStorage() {//creates a key and value in local storage to start creating a list of genreIds
+localStorage.setItem("genreIds","[]")//creates the key and empty value on page load
 }
-createEmptyStorage();
+createEmptyStorage();//calls the function just created
 
-buttonContainerEl.addEventListener("click", grabData);
-function grabData(event) 
+buttonContainerEl.addEventListener("click", grabData);//waits for a click on any genre button
+function grabData(event) //happens when a click on any genre button happens
 { 
 var localGenreIds= JSON.parse(localStorage.getItem("genreIds"));//this is an array list of genre ids already in the local storage
-var genreID = event.target.dataset.genreid;//gets the id code from the button that was clicked
-localGenreIds.push(genreID)//this will push genreID onto the end of the list from local storage. 
-console.log("the genreID after a click:  "+ genreID);
-localStorage.setItem("genreIds", JSON.stringify(localGenreIds));//localGenreIds is an array 
-//data typ
-var genreString= localGenreIds.toString();//turns 
+var genreID = event.target.dataset.genreid;//gets the genre ID from the button that was clicked
+localGenreIds.push(genreID)//pushes the genreID onto the end of the list from local storage. 
+localStorage.setItem("genreIds", JSON.stringify(localGenreIds));//localGenreIds is an array data type
+var genreString= localGenreIds.toString();//turns the genreString into an actual string data type
 console.log("the string in local storage: "+genreString)
 getTitleByGenre(genreString);//passes our first API call function the string of genre codes that it needs
 }
@@ -44,11 +40,11 @@ function getTitleByGenre(genreString) {
 			return response.json();
 		})
 		.then(function (genreObject) {
-			//console.log(genreObject+"is the genre parsed object")
+			console.log(genreObject+"is the genre parsed object")
 			//the parsed object is full of information like titles, overviews, and movie posters.
 			var randomIndex = Math.floor(Math.random() * genreObject.results.length);
 			//the randomIndex is like a bookmark that lets us pick a movie at random from a long list of movies and repeatedly come back to it to collect different pieces of information pertaining to that particular movie- specifically the title, original title, overview (movie summary), and vote average.
-			var title = genreObject.results[randomIndex].title;
+			//var title = genreObject.results[randomIndex].title;//do we even need this?
 			var movie_id = genreObject.results[randomIndex].id;
 			console.log("the movie ID: "+movie_id);
 			originalTitle = genreObject.results[randomIndex].original_title;
@@ -86,7 +82,7 @@ fetch(detailedURL, options3)
 //find the data we need in the object
 console.log(detailedObject);
 var IMDBID= detailedObject.imdb_id
-
+//if the IMDBID is null, then start over!
 console.log("The IMDBID IS :"+IMDBID);
 
 getStreamsByIMDBID(IMDBID); //call the final function that gets streaming data given an IMDBID.
@@ -95,10 +91,10 @@ getStreamsByIMDBID(IMDBID); //call the final function that gets streaming data g
 
 }
 
-function getStreamsByIMDBID(IMDBID){
+function getStreamsByIMDBID(IMDBID){//uses Get by IMDb ID which is an option from the MDBList API (paid for by Rhys)
 	var getByIMDBidURL = 
-	"https://mdblist.p.rapidapi.com/?i=" + IMDBID
-	console.log(getByIMDBidURL+"is the ImbdIdURL");
+	"https://mdblist.p.rapidapi.com/?i=" + IMDBID 
+	//console.log(getByIMDBidURL+"is the ImbdIdURL");
 	const options = {
 		method: 'GET',
 		headers: {
@@ -114,167 +110,22 @@ function getStreamsByIMDBID(IMDBID){
 		return response.json();
 	})
 	.then(function (imdbObject) {
-		console.log(imdbObject);
-		var firstStreamSource = imdbObject.streams[0].name //this will be undefined if the movie is still in theaters or whatever
-		console.log(firstStreamSource +" is the first streaming source"); //console logs the fist stream source, if one exists
-		var streams = imdbObject.streams
-		for (let i=0; i < streams.length; i++){//should repeatedly add stream sources to #streamSources (please make this in the HTML) . Hopefully it will append each stream source to the text, not everwrite whats there. If I recall correctly this will work correctly.
-			
+		//if the imdbObject is not an object for some weird (but weirdly frequent) reason, maybe start over with getTitleByGenre
+		console.log(imdbObject+" is the IMBDObject");
+			//if streams.length is not defined, start over
+			//if (typeof(imdbObject.streams[0].name) === 'undefined') {//if there is no first stream source (movie isn't streaming yet)
+			//	getTitleByGenre(genreString);//does getTitleByGenre have access to genreString here?? or do we need to pass it to getStream
+			//}else {
+			var streams = imdbObject.streams
+			for (let i=0; i < streams.length; i++){//should repeatedly add stream sources to #streamSources (please make this in the HTML) . Hopefully it will append each stream source to the text, not everwrite whats there. If I recall correctly this will work correctly.
 			var streamSources = imdbObject.streams[i].name
 			console.log("your movie is available on "+streamSources);
-			document.querySelector("#streamSources").textContent = streamSources;
+			document.querySelector("#streamSentence").textContent = streamSources;
+			//}
+			 trailerPath = imdbObject.trailer
+			document.querySelector("#vote_average2").textContent = trailerPath;
 			}
-			var trailerPath = imdbObject.trailer
-			document.querySelector("#trailerPath").textContent = trailerPath;
 		
 	});
 }
 
-
-//example code for that will be useful to rhys
-			//  document.querySelector("#streamSource").textContent = streamSource;
-			//  document.querySelector("#streamPrice").textContent = streamSource;
-			// document.querySelector("#ownership").textContent = ownership;
-
-			// for (let i = 0; i < sourcesObject.length; i++) {
-			// 	var streamSource = sourcesObject[i].name;
-
-			// 	var streamSentence =
-			// 		ownership +
-			// 		" this movie on " +
-			// 		streamSource +
-			// 		"------";
-			// 	console.log(streamSentence);
-			// 	document.querySelector("#streamSentence").textContent = streamSentence;
-			// }
-// function searchByTitle(title) {// BAD DATA! search by title seems to not be giving correcct data, so i'm going to abandon this one. 
-// 	var whereStreamingURL = //this is the url that we need for the next API call
-// 	'https://mdblist.p.rapidapi.com/?s='+title+"'"
-// 	const options = {
-// 		method: "GET",
-// 		headers: {			
-
-// 			'X-RapidAPI-Key': 'ab5fb0b08dmsh801b30df51c049dp15ea7ejsn09d021675790',//Rhys' X-RapidAPI- Key
-// 			'X-RapidAPI-Host': 'mdblist.p.rapidapi.com'
-// 		},
-// 	};
-// 	fetch(whereStreamingURL, options)
-// 		.then(function (response) {
-// 			//console.log(response);// could it be that the response is being sent differently?
-// 			if (!response.ok) { 
-// 				throw response.json();
-// 			}
-// 			//return response.body;
-// 			return response.json();
-// 		})
-// 		.then(function (ImdbObject) {
-// 			const parseImdbObject = JSON.parse(imdbObject)
-// 			var streams = parseImdbObject.streams[0].name
-// 		console.log(ImdbObject+"is the Object");
-// 		console.log(streams+" is the streams at [0]");
-
-// 			console.log(streams +" is the first streaming source");
-// 		});
-// }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-	
-
-//expand for list of genre codes
-// {
-// 	"genres": [
-// 	  {
-// 		"id": 28,
-// 		"name": "Action"
-// 	  },
-// 	  {
-// 		"id": 12,
-// 		"name": "Adventure"
-// 	  },
-// 	  {
-// 		"id": 16,
-// 		"name": "Animation"
-// 	  },
-// 	  {
-// 		"id": 35,
-// 		"name": "Comedy"
-// 	  },
-// 	  {
-// 		"id": 80,
-// 		"name": "Crime"
-// 	  },
-// 	  {
-// 		"id": 99,
-// 		"name": "Documentary"
-// 	  },
-// 	  {
-// 		"id": 18,
-// 		"name": "Drama"
-// 	  },
-// 	  {
-// 		"id": 10751,
-// 		"name": "Family"
-// 	  },
-// 	  {
-// 		"id": 14,
-// 		"name": "Fantasy"
-// 	  },
-// 	  {
-// 		"id": 36,
-// 		"name": "History"
-// 	  },
-// 	  {
-// 		"id": 27,
-// 		"name": "Horror"
-// 	  },
-// 	  {
-// 		"id": 10402,
-// 		"name": "Music"
-// 	  },
-// 	  {
-// 		"id": 9648,
-// 		"name": "Mystery"
-// 	  },
-// 	  {
-// 		"id": 10749,
-// 		"name": "Romance"
-// 	  },
-// 	  {
-// 		"id": 878,
-// 		"name": "Science Fiction"
-// 	  },
-// 	  {
-// 		"id": 10770,
-// 		"name": "TV Movie"
-// 	  },
-// 	  {
-// 		"id": 53,
-// 		"name": "Thriller"
-// 	  },
-// 	  {
-// 		"id": 10752,
-// 		"name": "War"
-// 	  },
-// 	  {
-// 		"id": 37,
-// 		"name": "Western"
-// 	  }
-// 	]
-//   }
