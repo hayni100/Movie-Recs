6336e892459368b4d65197bc957b58d3f644c9f6
Haein Kim <hayni100@gmail.com>
2022-11-14 16:47:52 -0800

    modalgit add -A

--- assets/js/Haein.js
+++ assets/js/Haein.js
@@ -5,7 +5,7 @@ var suggestedMovie = document.querySelector("#your-movie");
 var watchMovieBtn = document.querySelector("#watch-movie");
 var streamingAvail = document.querySelector("#streaming-avail");
 var startOverBtn = document.querySelector("#start-over");
-var savedMovieBtn = document.querySelector("#open-saved-movie");
+var savedMovieBtn = document.querySelector("#saved-movie");
 var modal = document.querySelector(".modal");
 var closeModal = document.querySelector("#close-modal");
 
@@ -34,51 +34,16 @@ function scrollTop() {
     }
 }
 
-
-
-modal.style.display = "none";
-
-
-//opening and closing modal for saved movie history//
-
-savedMovieBtn.addEventListener("click", function() { 
-   console.log("modal opened");
-   modal.style.display = "block";
+savedMovieBtn.addEventListener("click", function () {
+    console.log("open modal");
+    modal.classList.add("is-active");
+    modal.classList.add("is-clipped");
 });
 
-closeModal.addEventListener("click", function() {
+closeModal.addEventListener("click", function () {
     console.log("close modal");
-    modal.style.display = "none";
+    modal.classList.remove("is-active");
+    modal.classList.remove("is-clipped");
 });
 
-
-//////////////////
-// document.addEventListener(
-    
-//       $el.classList.add('is-active');
-//     }
-  
-//     function closeModal($el) {
-//       $el.classList.remove('is-active');
-//     }
-  
-//     function closeAllModals() {
-//       (document.querySelectorAll('.modal') || []).forEach(($modal) => {
-//         closeModal($modal);
-//       });
-//     }
-
-// //toggle movie suggestion page//
-// // suggestBtn.addEventListener("click",movieSuggest());
-
-// // function movieSuggest() {
-// //     var posterSection = document.querySelector(".poster-section");
-
-// //     posterSection.style.display = "none";
-
-
-// //     // if (posterSection.style.display === "block") {
-// //     //   posterSection.style.display = "none";
-// //     // } else {
-// //     //   posterSection.style.display = "block";
-//     // }
+ 
